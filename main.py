3add2bbd7c9add20424de372787f267d0257c9e2
parthDhankecha <pmd3290@gmail.com>
2024-11-14 14:08:18 +0530

    Change in port

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ os.environ["OLLAMA_NUM_THREADS"] = "4"
 # Verify that the environment variable is set
 print("OLLAMA_NUM_THREADS is set to:", os.getenv("OLLAMA_NUM_THREADS"))
 
-uvicorn.run("api:app", host="0.0.0.0", port=3002)
+uvicorn.run("api:app", host="0.0.0.0", port=3000)
